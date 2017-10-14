350b2e9a0d1e39b7dde1e3f082156a3b81b34ecf
Philipp Hartwig <ph@phhart.de>
2017-10-14 12:33:44 +0200

    Start to replace unidiomatic closures with structs
    Also properly encapsulate challenge secrets.

--- challenges/src/set3.rs
+++ challenges/src/set3.rs
@@ -27,26 +27,49 @@ use helper::ceil_div;
 
 use errors::*;
 
-fn encoder_3_17(key: &[u8]) -> Result<(Vec<u8>, Vec<u8>)> {
-    let inputs = [
-        "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
-        "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
-        "MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
-        "MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
-        "MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
-        "MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
-        "MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
-        "MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
-        "MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
-        "MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93",
-    ];
-    let mut rng = rand::thread_rng();
-    let input_index = rng.gen_range(0, 10);
-    let input = from_base64(inputs[input_index]).unwrap();
-    let iv = random_block();
-    let ciphertext = input.encrypt(key, Some(&iv), MODE::CBC)?;
-    Ok((ciphertext, iv))
+struct Server17 {
+    key: Vec<u8>
 }
+
+impl Server17 {
+    fn new() -> Self {
+        Self { key: random_block() }
+    }
+
+    fn get_session_token(&self) -> Result<(Vec<u8>, Vec<u8>)> {
+        let inputs = [
+            "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
+            "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
+            "MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
+            "MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
+            "MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
+            "MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
+            "MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
+            "MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
+            "MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
+            "MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93",
+        ];
+        let mut rng = rand::thread_rng();
+        let input_index = rng.gen_range(0, 10);
+        let input = from_base64(inputs[input_index]).unwrap();
+        let iv = random_block();
+        let ciphertext = input.encrypt(&self.key, Some(&iv), MODE::CBC)?;
+        Ok((ciphertext, iv))
+    }
+
+    fn is_padding_valid(&self, iv: &[u8], ciphertext: &[u8]) -> Result<bool> {
+        match ciphertext.decrypt(&self.key, Some(iv), MODE::CBC) {
+            Err(aes::Error(aes::ErrorKind::InvalidPadding, _)) => Ok(false),
+            Err(e) => Err(e.into()),
+            _ => Ok(true),
+        }
+    }
+
+    fn verify_solution(&self, cleartext: &[u8], iv: &[u8], ciphertext: &[u8]) -> Result<()> {
+        compare(cleartext, &ciphertext.decrypt(&self.key, Some(&iv), MODE::CBC)?)
+    }
+}
+
 
 fn random_block() -> Vec<u8> {
     let mut rng = rand::thread_rng();
@@ -54,16 +77,8 @@ fn random_block() -> Vec<u8> {
 }
 
 fn matasano3_17() -> Result<()> {
-    let key = random_block();
-    let oracle = |iv: &[u8], ciphertext: &[u8]| -> Result<bool> {
-        match ciphertext.decrypt(&key, Some(iv), MODE::CBC) {
-            Err(aes::Error(aes::ErrorKind::InvalidPadding, _)) => Ok(false),
-            Err(e) => Err(e.into()),
-            _ => Ok(true),
-        }
-    };
-
-    let (ciphertext, iv) = encoder_3_17(&key)?;
+    let server = Server17::new();
+    let (ciphertext, iv) = server.get_session_token()?;
     let mut cleartext = vec![0; ciphertext.len()];
     let mut prev = iv.clone();
     for (block_count, block) in ciphertext.chunks(BLOCK_SIZE).enumerate() {
@@ -74,10 +89,9 @@ fn matasano3_17() -> Result<()> {
             //Awaits replacement for range_inclusive
             for u in all_bytes() {
                 prev[i] ^= u;
-                //let foo: Result<bool>= oracle(&prev, block);
-                if oracle(&prev, block)? && (i < BLOCK_SIZE - 1 || {
+                if server.is_padding_valid(&prev, block)? && (i < BLOCK_SIZE - 1 || {
                     prev[i - 1] ^= 1;
-                    let result = oracle(&prev, block)?;
+                    let result = server.is_padding_valid(&prev, block)?;
                     prev[i - 1] ^= 1;
                     result
                 }) {
@@ -90,7 +104,7 @@ fn matasano3_17() -> Result<()> {
         prev = block.to_vec();
     }
     unpad_inplace(&mut cleartext, BLOCK_SIZE as u8)?;
-    compare(cleartext, ciphertext.decrypt(&key, Some(&iv), MODE::CBC)?)
+    server.verify_solution(&cleartext, &ciphertext, &iv)
 }
 
 fn matasano3_18() -> Result<()> {
