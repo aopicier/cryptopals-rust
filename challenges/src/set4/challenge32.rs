e4b310c8d501d1539083156f9ec8d1688a481600
Philipp Hartwig <ph@phhart.de>
2018-09-22 22:15:54 +0200

    Print message instead of disabling challenge

--- challenges/src/set4/challenge32.rs
+++ challenges/src/set4/challenge32.rs
@@ -7,12 +7,7 @@ use rand::Rng;
 use errors::*;
 
 pub fn run() -> Result<(), Error> {
-    let skip_exercise = true;
-
-    if skip_exercise {
-        return Err(ChallengeError::Skipped("Runs very long.").into());
-    }
-
+    println!("Challenge 32: takes about three minutes, pleases wait ...");
     let mut rng = rand::thread_rng();
     let key: Vec<u8> = rng.gen_iter().take(20).collect();
 
